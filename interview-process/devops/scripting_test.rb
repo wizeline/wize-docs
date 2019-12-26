3155e21215024dfa827a321f05839d9031a9671f
Saul Ortigoza <saul.ortigoza@wizeline.com>
2019-12-26 12:59:59 -0800

    update/devops-questions

--- interview-process/devops/scripting_test.rb
+++ interview-process/devops/scripting_test.rb
@@ -12,18 +12,19 @@ end
 File.open("example.csv", "w") { |f| f.write csv_string }
 puts csv_string
 
-# Transpose the CSV file to it is:
+# Transpose the CSV file so it is:
 # name:saul:leon:carlos
 # email:saul@wizeline.com:leon@wizeline.com:carlos@wizeline.com
 # cellphone:11111111:22222222:33333333
 
 # # Bash version
-# cat <<EOF > example.csv
-# name,email,cellphone
-# saul,saul@wizeline.com,11111111
-# leon,leon@wizeline.com,22222222
-# carlos,carlos@wizeline.com,33333333
-# EOF
-# cat example.csv
+cat <<EOF > example.csv
+name,email,cellphone
+saul,saul@wizeline.com,11111111
+leon,leon@wizeline.com,22222222
+carlos,carlos@wizeline.com,33333333
+EOF
+cat example.csv
 
+# -- begin script --
 
